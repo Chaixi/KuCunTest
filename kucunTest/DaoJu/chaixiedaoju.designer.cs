a6442a8bb68f615fd71169bb9380d3a1007063d6
Chaixi <chaixi1103@foxmail.com>
2017-09-24 13:30:18 +0800

    拆卸刀具微调

--- kucunTest/DaoJu/chaixiedaoju.designer.cs
+++ kucunTest/DaoJu/chaixiedaoju.designer.cs
@@ -28,12 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.daojuid = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lbjmx = new System.Windows.Forms.DataGridView();
+            this.lbjmc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbjxh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbjgg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dw = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kcsl = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -42,31 +48,28 @@
             this.daojuguige = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.daojuxinghao = new System.Windows.Forms.TextBox();
-            this.lbjmc = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lbjxh = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lbjgg = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.sl = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dw = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.kcsl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.lbjmx)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // daojuid
             // 
             this.daojuid.Enabled = false;
-            this.daojuid.Location = new System.Drawing.Point(355, 107);
+            this.daojuid.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.daojuid.Location = new System.Drawing.Point(444, 80);
             this.daojuid.Margin = new System.Windows.Forms.Padding(5);
             this.daojuid.Name = "daojuid";
             this.daojuid.ReadOnly = true;
-            this.daojuid.Size = new System.Drawing.Size(139, 21);
+            this.daojuid.Size = new System.Drawing.Size(177, 29);
             this.daojuid.TabIndex = 15;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.label3.Location = new System.Drawing.Point(285, 105);
+            this.label3.Location = new System.Drawing.Point(360, 86);
             this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 21);
@@ -77,7 +80,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.label2.Location = new System.Drawing.Point(40, 57);
+            this.label2.Location = new System.Drawing.Point(24, 36);
             this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 21);
@@ -91,7 +94,7 @@
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(590, 52);
+            this.label1.Size = new System.Drawing.Size(704, 52);
             this.label1.TabIndex = 11;
             this.label1.Text = "拆卸刀具";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -100,15 +103,16 @@
             // 
             this.lbjmx.AllowUserToAddRows = false;
             this.lbjmx.AllowUserToDeleteRows = false;
+            this.lbjmx.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.lbjmx.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.lbjmx.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.lbjmx.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.lbjmx.ColumnHeadersHeight = 32;
             this.lbjmx.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.lbjmc,
@@ -117,19 +121,71 @@
             this.sl,
             this.dw,
             this.kcsl});
-            this.lbjmx.Location = new System.Drawing.Point(10, 24);
+            this.lbjmx.Location = new System.Drawing.Point(10, 32);
             this.lbjmx.Margin = new System.Windows.Forms.Padding(5);
             this.lbjmx.Name = "lbjmx";
             this.lbjmx.ReadOnly = true;
             this.lbjmx.RowTemplate.Height = 23;
             this.lbjmx.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.lbjmx.Size = new System.Drawing.Size(555, 212);
+            this.lbjmx.Size = new System.Drawing.Size(656, 204);
             this.lbjmx.TabIndex = 0;
+            // 
+            // lbjmc
+            // 
+            this.lbjmc.DataPropertyName = "lbjmc";
+            this.lbjmc.FillWeight = 107.9076F;
+            this.lbjmc.HeaderText = "零部件名称";
+            this.lbjmc.Name = "lbjmc";
+            this.lbjmc.ReadOnly = true;
+            this.lbjmc.Width = 115;
+            // 
+            // lbjxh
+            // 
+            this.lbjxh.DataPropertyName = "lbjxh";
+            this.lbjxh.FillWeight = 128.4288F;
+            this.lbjxh.HeaderText = "零部件型号";
+            this.lbjxh.Name = "lbjxh";
+            this.lbjxh.ReadOnly = true;
+            this.lbjxh.Width = 115;
+            // 
+            // lbjgg
+            // 
+            this.lbjgg.DataPropertyName = "lbjgg";
+            this.lbjgg.FillWeight = 121.3444F;
+            this.lbjgg.HeaderText = "零部件规格";
+            this.lbjgg.Name = "lbjgg";
+            this.lbjgg.ReadOnly = true;
+            this.lbjgg.Width = 115;
+            // 
+            // sl
+            // 
+            this.sl.DataPropertyName = "sl";
+            this.sl.FillWeight = 97.40771F;
+            this.sl.HeaderText = "数量";
+            this.sl.Name = "sl";
+            this.sl.ReadOnly = true;
+            this.sl.Width = 67;
+            // 
+            // dw
+            // 
+            this.dw.DataPropertyName = "dw";
+            this.dw.HeaderText = "单位";
+            this.dw.Name = "dw";
+            this.dw.ReadOnly = true;
+            this.dw.Width = 67;
+            // 
+            // kcsl
+            // 
+            this.kcsl.DataPropertyName = "kcsl";
+            this.kcsl.HeaderText = "库存数量";
+            this.kcsl.Name = "kcsl";
+            this.kcsl.ReadOnly = true;
+            this.kcsl.Width = 99;
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(305, 420);
+            this.button2.Location = new System.Drawing.Point(354, 449);
             this.button2.Margin = new System.Windows.Forms.Padding(5);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 40);
@@ -141,7 +197,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(146, 420);
+            this.button1.Location = new System.Drawing.Point(195, 449);
             this.button1.Margin = new System.Windows.Forms.Padding(5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 40);
@@ -154,11 +210,11 @@
             // 
             this.groupBox1.Controls.Add(this.lbjmx);
             this.groupBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.groupBox1.Location = new System.Drawing.Point(1, 164);
+            this.groupBox1.Location = new System.Drawing.Point(14, 193);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
-            this.groupBox1.Size = new System.Drawing.Size(575, 246);
+            this.groupBox1.Size = new System.Drawing.Size(676, 246);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "零部件信息";
@@ -166,18 +222,19 @@
             // daojuleixing
             // 
             this.daojuleixing.Enabled = false;
-            this.daojuleixing.Location = new System.Drawing.Point(124, 57);
+            this.daojuleixing.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.daojuleixing.Location = new System.Drawing.Point(108, 33);
             this.daojuleixing.Margin = new System.Windows.Forms.Padding(5);
             this.daojuleixing.Name = "daojuleixing";
             this.daojuleixing.ReadOnly = true;
-            this.daojuleixing.Size = new System.Drawing.Size(139, 21);
+            this.daojuleixing.Size = new System.Drawing.Size(183, 29);
             this.daojuleixing.TabIndex = 18;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.label4.Location = new System.Drawing.Point(280, 60);
+            this.label4.Location = new System.Drawing.Point(360, 36);
             this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(74, 21);
@@ -187,18 +244,19 @@
             // daojuguige
             // 
             this.daojuguige.Enabled = false;
-            this.daojuguige.Location = new System.Drawing.Point(355, 60);
+            this.daojuguige.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.daojuguige.Location = new System.Drawing.Point(444, 33);
             this.daojuguige.Margin = new System.Windows.Forms.Padding(5);
             this.daojuguige.Name = "daojuguige";
             this.daojuguige.ReadOnly = true;
-            this.daojuguige.Size = new System.Drawing.Size(139, 21);
+            this.daojuguige.Size = new System.Drawing.Size(177, 29);
             this.daojuguige.TabIndex = 20;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("微软雅黑", 12F);
-            this.label5.Location = new System.Drawing.Point(40, 107);
+            this.label5.Location = new System.Drawing.Point(24, 83);
             this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(74, 21);
@@ -208,76 +266,38 @@
             // daojuxinghao
             // 
             this.daojuxinghao.Enabled = false;
-            this.daojuxinghao.Location = new System.Drawing.Point(124, 107);
+            this.daojuxinghao.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.daojuxinghao.Location = new System.Drawing.Point(108, 80);
             this.daojuxinghao.Margin = new System.Windows.Forms.Padding(5);
             this.daojuxinghao.Name = "daojuxinghao";
             this.daojuxinghao.ReadOnly = true;
-            this.daojuxinghao.Size = new System.Drawing.Size(139, 21);
+            this.daojuxinghao.Size = new System.Drawing.Size(183, 29);
             this.daojuxinghao.TabIndex = 22;
             // 
-            // lbjmc
+            // groupBox2
             // 
-            this.lbjmc.DataPropertyName = "lbjmc";
-            this.lbjmc.FillWeight = 107.9076F;
-            this.lbjmc.HeaderText = "零部件名称";
-            this.lbjmc.Name = "lbjmc";
-            this.lbjmc.ReadOnly = true;
-            this.lbjmc.Width = 101;
-            // 
-            // lbjxh
-            // 
-            this.lbjxh.DataPropertyName = "lbjxh";
-            this.lbjxh.FillWeight = 128.4288F;
-            this.lbjxh.HeaderText = "零部件型号";
-            this.lbjxh.Name = "lbjxh";
-            this.lbjxh.ReadOnly = true;
-            // 
-            // lbjgg
-            // 
-            this.lbjgg.DataPropertyName = "lbjgg";
-            this.lbjgg.FillWeight = 121.3444F;
-            this.lbjgg.HeaderText = "零部件规格";
-            this.lbjgg.Name = "lbjgg";
-            this.lbjgg.ReadOnly = true;
-            // 
-            // sl
-            // 
-            this.sl.DataPropertyName = "sl";
-            this.sl.FillWeight = 97.40771F;
-            this.sl.HeaderText = "数量";
-            this.sl.Name = "sl";
-            this.sl.ReadOnly = true;
-            this.sl.Width = 71;
-            // 
-            // dw
-            // 
-            this.dw.DataPropertyName = "dw";
-            this.dw.HeaderText = "单位";
-            this.dw.Name = "dw";
-            this.dw.ReadOnly = true;
-            this.dw.Width = 60;
-            // 
-            // kcsl
-            // 
-            this.kcsl.DataPropertyName = "kcsl";
-            this.kcsl.HeaderText = "库存数量";
-            this.kcsl.Name = "kcsl";
-            this.kcsl.ReadOnly = true;
-            this.kcsl.Width = 80;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.daojuxinghao);
+            this.groupBox2.Controls.Add(this.daojuid);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.daojuleixing);
+            this.groupBox2.Controls.Add(this.daojuguige);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.Location = new System.Drawing.Point(14, 55);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(676, 121);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "刀具信息";
             // 
             // chaixiedaoju
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 472);
-            this.Controls.Add(this.daojuxinghao);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.daojuguige);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.daojuleixing);
-            this.Controls.Add(this.daojuid);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(704, 507);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -287,8 +307,9 @@
             this.Load += new System.EventHandler(this.chaixiedaoju_Load);
             ((System.ComponentModel.ISupportInitialize)(this.lbjmx)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -313,5 +334,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn sl;
         private System.Windows.Forms.DataGridViewTextBoxColumn dw;
         private System.Windows.Forms.DataGridViewTextBoxColumn kcsl;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
