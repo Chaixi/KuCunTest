0f609f0653307f20660330818634578ece9119e7
Chaixi <chaixi1103@foxmail.com>
2017-11-07 14:34:39 +0800

    新增零部件修改库存界面

---
+++ kucunTest/LingBuJian/lbj_xiugaikucun.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace kucunTest.LingBuJian
+{
+    partial class lbj_xiugaikucun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_comfirm = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LBJMC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LBJGG = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LBJXH = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.KCWZ = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.KCSL = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.BZ = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.DW1 = new System.Windows.Forms.TextBox();
+            this.XGSL = new System.Windows.Forms.TextBox();
+            this.DW2 = new System.Windows.Forms.TextBox();
+            this.rb_add = new System.Windows.Forms.RadioButton();
+            this.rb_sub = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btn_comfirm
+            // 
+            this.btn_comfirm.Location = new System.Drawing.Point(173, 242);
+            this.btn_comfirm.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_comfirm.Name = "btn_comfirm";
+            this.btn_comfirm.Size = new System.Drawing.Size(100, 31);
+            this.btn_comfirm.TabIndex = 0;
+            this.btn_comfirm.Text = "确认";
+            this.btn_comfirm.UseVisualStyleBackColor = true;
+            this.btn_comfirm.Click += new System.EventHandler(this.btn_comfirm_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(297, 242);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(100, 31);
+            this.btn_close.TabIndex = 0;
+            this.btn_close.Text = "取消";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "零部件名称";
+            // 
+            // LBJMC
+            // 
+            this.LBJMC.Location = new System.Drawing.Point(125, 40);
+            this.LBJMC.Margin = new System.Windows.Forms.Padding(4);
+            this.LBJMC.Name = "LBJMC";
+            this.LBJMC.Size = new System.Drawing.Size(132, 26);
+            this.LBJMC.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "零部件规格";
+            // 
+            // LBJGG
+            // 
+            this.LBJGG.Location = new System.Drawing.Point(125, 76);
+            this.LBJGG.Margin = new System.Windows.Forms.Padding(4);
+            this.LBJGG.Name = "LBJGG";
+            this.LBJGG.Size = new System.Drawing.Size(132, 26);
+            this.LBJGG.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 115);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "零部件型号";
+            // 
+            // LBJXH
+            // 
+            this.LBJXH.Location = new System.Drawing.Point(124, 112);
+            this.LBJXH.Margin = new System.Windows.Forms.Padding(4);
+            this.LBJXH.Name = "LBJXH";
+            this.LBJXH.Size = new System.Drawing.Size(132, 26);
+            this.LBJXH.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 153);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "库存位置";
+            // 
+            // KCWZ
+            // 
+            this.KCWZ.Location = new System.Drawing.Point(125, 150);
+            this.KCWZ.Margin = new System.Windows.Forms.Padding(4);
+            this.KCWZ.Name = "KCWZ";
+            this.KCWZ.Size = new System.Drawing.Size(132, 26);
+            this.KCWZ.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 189);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "库存数量";
+            // 
+            // KCSL
+            // 
+            this.KCSL.Location = new System.Drawing.Point(125, 186);
+            this.KCSL.Margin = new System.Windows.Forms.Padding(4);
+            this.KCSL.Name = "KCSL";
+            this.KCSL.Size = new System.Drawing.Size(74, 26);
+            this.KCSL.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 82);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "修改数量";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(294, 121);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 16);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "修改说明";
+            // 
+            // BZ
+            // 
+            this.BZ.Location = new System.Drawing.Point(374, 121);
+            this.BZ.Margin = new System.Windows.Forms.Padding(4);
+            this.BZ.Multiline = true;
+            this.BZ.Name = "BZ";
+            this.BZ.Size = new System.Drawing.Size(133, 90);
+            this.BZ.TabIndex = 2;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(294, 44);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 16);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "操作类型";
+            // 
+            // DW1
+            // 
+            this.DW1.Location = new System.Drawing.Point(206, 185);
+            this.DW1.Margin = new System.Windows.Forms.Padding(4);
+            this.DW1.Name = "DW1";
+            this.DW1.Size = new System.Drawing.Size(51, 26);
+            this.DW1.TabIndex = 2;
+            // 
+            // XGSL
+            // 
+            this.XGSL.Location = new System.Drawing.Point(374, 79);
+            this.XGSL.Margin = new System.Windows.Forms.Padding(4);
+            this.XGSL.Name = "XGSL";
+            this.XGSL.Size = new System.Drawing.Size(74, 26);
+            this.XGSL.TabIndex = 2;
+            // 
+            // DW2
+            // 
+            this.DW2.Location = new System.Drawing.Point(456, 79);
+            this.DW2.Margin = new System.Windows.Forms.Padding(4);
+            this.DW2.Name = "DW2";
+            this.DW2.Size = new System.Drawing.Size(51, 26);
+            this.DW2.TabIndex = 2;
+            // 
+            // rb_add
+            // 
+            this.rb_add.AutoSize = true;
+            this.rb_add.Checked = true;
+            this.rb_add.Location = new System.Drawing.Point(374, 43);
+            this.rb_add.Name = "rb_add";
+            this.rb_add.Size = new System.Drawing.Size(58, 20);
+            this.rb_add.TabIndex = 3;
+            this.rb_add.TabStop = true;
+            this.rb_add.Text = "添加";
+            this.rb_add.UseVisualStyleBackColor = true;
+            this.rb_add.CheckedChanged += new System.EventHandler(this.rb_add_CheckedChanged);
+            // 
+            // rb_sub
+            // 
+            this.rb_sub.AutoSize = true;
+            this.rb_sub.Location = new System.Drawing.Point(448, 43);
+            this.rb_sub.Name = "rb_sub";
+            this.rb_sub.Size = new System.Drawing.Size(58, 20);
+            this.rb_sub.TabIndex = 3;
+            this.rb_sub.Text = "减少";
+            this.rb_sub.UseVisualStyleBackColor = true;
+            this.rb_sub.CheckedChanged += new System.EventHandler(this.rb_sub_CheckedChanged);
+            // 
+            // lbj_xiugaikucun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 299);
+            this.Controls.Add(this.rb_sub);
+            this.Controls.Add(this.rb_add);
+            this.Controls.Add(this.BZ);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DW2);
+            this.Controls.Add(this.XGSL);
+            this.Controls.Add(this.DW1);
+            this.Controls.Add(this.KCSL);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.KCWZ);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LBJXH);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LBJGG);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LBJMC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_comfirm);
+            this.Font = new System.Drawing.Font("宋体", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "lbj_xiugaikucun";
+            this.Text = "修改库存";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_comfirm;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LBJMC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LBJGG;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox LBJXH;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox KCWZ;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox KCSL;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox BZ;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox DW1;
+        private System.Windows.Forms.TextBox XGSL;
+        private System.Windows.Forms.TextBox DW2;
+        private System.Windows.Forms.RadioButton rb_add;
+        private System.Windows.Forms.RadioButton rb_sub;
+    }
+}
---
+++ kucunTest/LingBuJian/lbj_xiugaikucun.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kucunTest.LingBuJian
+{
+    public partial class lbj_xiugaikucun : Form
+    {
+        string SqlStr = "";
+        MySql SQL = new MySql();
+
+        public lbj_xiugaikucun()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 确认按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_comfirm_Click(object sender, EventArgs e)
+        {
+            string tishi = "";
+            string liushui_sqlstr = "";
+
+            if (XGSL.Text != "" && XGSL != null)
+            {
+                if(rb_add.Checked)
+                {
+                    SqlStr = string.Format("UPDATE {0} SET WHERE ");
+                    liushui_sqlstr = string.Format("INSERT INTO {0}() VALUES('{1}')");
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                tishi = "请填写要操作的数量！";
+            }
+
+            if(tishi != "")
+            {
+                SQL.ExecuteNonQuery(SqlStr);
+                SQL.ExecuteNonQuery(liushui_sqlstr);
+                tishi = "更新成功！";
+            }
+
+            MessageBox.Show(tishi, "提示");
+        }
+
+        /// <summary>
+        /// 取消按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void rb_add_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rb_add.Checked)
+            {
+                rb_sub.Checked = false;
+            }
+        }
+
+        private void rb_sub_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb_sub.Checked)
+            {
+                rb_add.Checked = false;
+            }
+        }
+    }
+}
